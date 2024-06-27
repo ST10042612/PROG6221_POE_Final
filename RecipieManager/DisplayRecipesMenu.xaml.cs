824f5f07110b257e5e52cc93c80f9c54d09b33e0
Matthew Paes <104486473+ST10042612@users.noreply.github.com>
2024-06-27 23:01:49 +0200

    Update DisplayRecipesMenu.xaml.cs

--- RecipieManager/DisplayRecipesMenu.xaml.cs
+++ RecipieManager/DisplayRecipesMenu.xaml.cs
@@ -19,22 +19,23 @@ namespace RecipieManager
     /// </summary>
     public partial class DisplayRecipesMenu : Window
     {
-
+        
         Recipes rec;
         public DisplayRecipesMenu()
         {
             InitializeComponent();
         }
 
+        //loads all the recipes into a list box
         private void Recipe_Load(object sender, EventArgs e) //https://www.geeksforgeeks.org/how-to-add-items-in-listbox-in-c-sharp/
         {
 
-            RecBox.Items.Add("Recipie Name" + "   " + "Steps" + "   " + "Ingredients");
-
+            RecBox.Items.Add("Recipie Name" + "   " + "Steps" + "   " + "Ingredients");// header
+            
             List<string> tempRecipeName = rec.recipeName;
             tempRecipeName.Sort();
 
-            for (int i = 0; i < rec.recipeName.Count(); i++)
+            for (int i = 0; i < rec.recipeName.Count(); i++) // goes through lists and add them into a string that will then be displayed
             {
 
                 string str = $"{rec.ingredientName[i],-11}{"",-4}{rec.ingredientName[i].Count(),-5}{"",-4}{rec.steps[i].Count(),-11}";
